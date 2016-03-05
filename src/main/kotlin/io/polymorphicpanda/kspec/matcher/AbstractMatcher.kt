052fc31634c4a46820ad062cccc4cbc015a93e75
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2016-03-05 23:58:59 +0800

    Improve assertion errors

--- src/main/kotlin/io/polymorphicpanda/kspec/matcher/AbstractMatcher.kt
+++ src/main/kotlin/io/polymorphicpanda/kspec/matcher/AbstractMatcher.kt
@@ -1,14 +1,6 @@
 package io.polymorphicpanda.kspec.matcher
 
 abstract class AbstractMatcher<T>(val message: Any?): Matcher<T> {
-    protected fun test(expression: Boolean) {
-        if (message != null) {
-            assert(expression, { message })
-        } else {
-            assert(expression)
-        }
-    }
-
     companion object {
         fun fail(message: Any? = null) {
             if (message == null) {
--- src/main/kotlin/io/polymorphicpanda/kspec/matcher/SimpleMatcher.kt
+++ src/main/kotlin/io/polymorphicpanda/kspec/matcher/SimpleMatcher.kt
@@ -1,8 +1,23 @@
 package io.polymorphicpanda.kspec.matcher
 
+import junit.framework.ComparisonFailure
+
 abstract class SimpleMatcher<T>(val expected: T, message: Any?): AbstractMatcher<T>(message) {
     override fun match(arg: T?) {
-        test(doAssert(arg, expected))
+        test(doAssert(arg, expected)) {
+            val desc = if (message != null) {
+                "$message"
+            } else {
+                "expected:<[$expected]> but was:<[$arg]>"
+            }
+            ComparisonFailure(desc, "$expected", "$arg")
+        }
+    }
+
+    protected fun test(expression: Boolean, block: () -> Throwable) {
+        if (!expression) {
+            throw block()
+        }
     }
 
     protected abstract fun doAssert(actual: T?, expected: T): Boolean
